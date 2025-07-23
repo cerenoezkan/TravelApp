8ae3c84d21186432e52fbfd057e2593eb93cab89
Ceren Özkan <cer70en@gmail.com>
2025-07-23 20:03:37 +0300

    feat: build splash screen layout and add navigation to MainActivity

--- app/src/main/java/com/example/travelapp/SplashActivity.java
+++ app/src/main/java/com/example/travelapp/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.travelapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+import com.example.travelapp.databinding.ActivitySplashBinding;
 
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding=ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v ->
+                startActivity(new Intent(SplashActivity.this,MainActivity.class)));
     }
 }
